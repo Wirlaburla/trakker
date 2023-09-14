db19f85083c11d363f4e66301872fa7f3165a471
Wirlaburla <wirlaburla@worlio.com>
2023-09-14 00:58:12 -0500

    add forgotten header

---
+++ src/trakker.h
@@ -0,0 +1,15 @@
+#ifndef TRAKKER_H
+#define TRAKKER_H
+
+void destroyWindows();
+void createWindows();
+void renderInfo(xmp_module_info *mi, xmp_frame_info *fi);
+void renderAbout();
+void renderTrack(xmp_module_info *mi, xmp_frame_info *fi);
+void renderRows(xmp_module_info *mi, xmp_frame_info *fi);
+void renderChannels(xmp_module_info *mi, xmp_frame_info *fi);
+void renderInstruments(xmp_module_info *mi, xmp_frame_info *fi);
+void generateEffectsTable(xmp_module *xm);
+bool isPartOf(char* w1, char* w2);
+
+#endif
